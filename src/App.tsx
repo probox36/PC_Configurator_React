0624b42c8c059420c531f6b0685efe9e4ec556e0
probox36 <probox36@gmail.com>
2024-03-11 00:36:02 +0300

    Отрефакторил и максимально упростил App.tsx, сделал Part классом, починил оверлеи на карточках

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import { useState, cloneElement, ReactElement } from 'react';
 import './styles/style.App.css';
-import PartSlot from './PartSlot.tsx';
-import PartSelection from './PartsSelection.js';
+import PartSelection from './PartsSelection.tsx';
 import ItemCard from './ItemCard.tsx';
-import ItemCardHolder from './ItemCardHolder.js';
+import ItemCardHolder from './ItemCardHolder.tsx';
 import { Computer } from './entities/Computer.ts';
-import MotherboardIcon from './images/Motherboard.svg';
-import CPUIcon from './images/CPU.svg';
-import GPUIcon from './images/GPU.svg';
-import RAMIcon from './images/RAM.svg';
-import PowerIcon from './images/Power.svg';
-import HDDIcon from './images/HDD.svg';
-import FanIcon from './images/Fan.svg';
 import { fetchComponents } from "./Fetcher.ts";
 import { Part } from './entities/Part.ts';
 import React from 'react';
 import { PartClassName } from './entities/PartClassName.ts';
 import { Motherboard } from './entities/Motherboard.ts';
 import { Case } from './entities/Case.ts';
+import { predefinedPartSlotList } from './PredefinedValues.tsx';
 
 export let computer = new Computer();
 
@@ -25,16 +18,53 @@ function App() {
 
   const [totalCost, setTotalCost] = useState(0);
   const [itemCardHolderState, setItemCardHolderState] = useState('showNothing');
-  const [itemCardList, setItemcardList] = useState<Array<ReactElement>>([]); // todo: вот этот кал не обновляется и всегда возвращает пустой список
-
-  const updatePartSlotByClassName = (partClassName) => {
-    for (let i = 0; i < partSlotList.length; i++) {
-      if (partSlotList[i].props.partClassName === partClassName) {
-        partSlotList[i] = cloneElement(partSlotList[i], { computer: computer });
-      }
+  const [itemCardList, setItemcardList] = useState<Array<ReactElement>>([]);
+  
+  const countSimilarParts = (part: Part, existingPart: Part | Array<Part>): number => {
+    let count = 0;
+    if (Array.isArray(existingPart)) {
+      existingPart.forEach((elem) => {
+        if (elem.isEqual(part)) {
+          count++;
+        }
+      });
+    } else {
+      existingPart = existingPart as Part;
+      if (existingPart.isEqual(part)) { count++; }
     }
+    return count;
+  };
 
-    setPartSlotList(partSlotList);
+  const displayItemCards = (parts: Array<Part>) => {
+    const cardList: ReactElement[] = [];
+    
+    parts.forEach((part) => {
+
+      let existingPart = computer[part.partClassName.toString()];
+      let cardSelectedFlag = false;
+      let quantity = 0;
+
+      if (existingPart != undefined) {
+        quantity = countSimilarParts(part, existingPart);
+        cardSelectedFlag = quantity > 0;
+      } 
+
+      const isSingularFlag = !computer.multipleSelectionAvailable(part);
+      const multipleChoiceFlag = !isSingularFlag && cardSelectedFlag;
+      const cardActiveFlag = (cardSelectedFlag && isSingularFlag) || computer.getFreeSlotsLeft(part);
+
+      cardList.push((<ItemCard
+        key={1 + part.partClassName.substring(0, 1) + part.id}
+        partObject={part}
+        onClickAdd={onClickItemCard}
+
+        selected={cardSelectedFlag}
+        active={cardActiveFlag}
+        multipleChoice={multipleChoiceFlag}
+        quantity={quantity} />));
+    });
+
+    setItemcardList(cardList);
   };
 
   const onClickPartSlot = async (partClassName: PartClassName) => {
@@ -46,42 +76,8 @@ function App() {
       return;
     }
     if (reply.status) {
-      const selectedPart = computer[partClassName.toString()];
-      const cardList: ReactElement[] = [];
-      const partSelectedFlag = typeof selectedPart != 'undefined';
-      // if (typeof freeSlotsLeft === 'undefined') { freeSlotsLeft = 0; }
-      parts.forEach((part) => {
-
-        let freeSlotsLeft = computer.getFreeSlotsLeft(part);
-        let cardSelectedFlag;
-        let cardActiveFlag;
-        let multipleChoiceFlag = false;
-        if (partSelectedFlag) {
-          if (Array.isArray(selectedPart)) {
-            cardSelectedFlag = selectedPart.map((elem) => elem.id).includes(part.id);
-            multipleChoiceFlag = selectedPart.length > 0 && cardSelectedFlag;
-          } else {
-            cardSelectedFlag = part.id === selectedPart.id;
-          }
-          cardActiveFlag = (cardSelectedFlag && !multipleChoiceFlag) || freeSlotsLeft;
-        } else {
-          cardSelectedFlag = false;
-          cardActiveFlag = true;
-        }
-        
-        cardList.push((<ItemCard
-          key={'0' + part.id}
-          partObject={part}
-          onClickAdd={onClickItemCard}
-          selected={cardSelectedFlag}
-          active={cardActiveFlag}
-          multipleChoice={multipleChoiceFlag} />));
-        
-      });
-
-      setItemcardList(cardList);
+      displayItemCards(parts);
       setItemCardHolderState('showItemCards');
-
     } else {
       alert("Нет подключения к серверу");
     }
@@ -89,7 +85,6 @@ function App() {
   }
 
   const onClickItemCard = async (part: Part, addMode: boolean) => {
-    // запрашивает список компонентов у бд
     const reply = await fetchComponents(part.partClassName);
     const parts = reply.castTo(part.partClassName);
     if (reply.status) { 
@@ -110,7 +105,8 @@ function App() {
 
       } else {
         
-        if (part instanceof Motherboard) {
+        if (part instanceof Motherboard || part instanceof Case) {
+          // на этом этапе должен появляться modal
           computer = new Computer(); 
         } else {
           computer.removePart(part);
@@ -118,68 +114,16 @@ function App() {
 
       }
 
-      const isSingularFlag = !computer.multipleSelectionAvailable(part);
-      const noSlotsLeftFlag = !computer.getFreeSlotsLeft(part);
-      const cardList: ReactElement[] = [];
-
-      parts.forEach((part) => {
-        let cardSelectedFlag;
-        let cardActiveFlag;
-        let multipleChoiceFlag;
-        const existingPart = computer[part.partClassName.toString()];
-
-        // узнает, выбрана ли карточка
-        // figures out if itemCard is chosen by user
-        if (typeof existingPart != 'undefined' && existingPart != null) {
-          if (Array.isArray(existingPart) && existingPart.length > 0) {
-            cardSelectedFlag = existingPart.map((elem) => elem.id).includes(part.id);
-          } else {
-            cardSelectedFlag = part.id === existingPart.id;
-          }
-        } 
-
-        multipleChoiceFlag = !isSingularFlag && cardSelectedFlag;
-
-        // узнает, активна ли карточка
-        // figures out if itemCard is active
-        if (addMode) {
-          cardActiveFlag = (cardSelectedFlag && isSingularFlag) || !noSlotsLeftFlag;
-        } else {
-          cardActiveFlag = true;
-        }
-
-        // создает пустой массив, в него ложит карточки с новыми параметрами
-        // creates an empty array and fills it with itemCards with new props
-        cardList.push((<ItemCard
-          key={addMode ? 1 : 2 + part.partClassName.substring(0, 1) + part.id}
-          partObject={part}
-          onClickAdd={onClickItemCard}
-          selected={cardSelectedFlag}
-          active={cardActiveFlag}
-          multipleChoice={multipleChoiceFlag} />));
-      });
-      // заменяет itemCardList на этот массив
-      // replaces itemCardList with that array
-      setItemcardList(cardList);
+      displayItemCards(parts);
       setTotalCost(computer.calculateTotalCost());
-      updatePartSlotByClassName(part.partClassName);
+      setPartSlotList(predefinedPartSlotList(computer, onClickPartSlot))
 
     } else {
       alert("Нет подключения к серверу");
     }
   };
 
-  const [partSlotList, setPartSlotList] = useState(
-    [
-      (<PartSlot key={1} iconAddress={MotherboardIcon} partClassName={PartClassName.Motherboard} computer={computer} setOnClick={onClickPartSlot} partName={"Материнская плата"}/>),
-      (<PartSlot key={2} iconAddress={CPUIcon} partClassName={PartClassName.CPU} computer={computer} setOnClick={onClickPartSlot} partName={"Процессор"}/>),
-      (<PartSlot key={3} iconAddress={GPUIcon} partClassName={PartClassName.GPU} computer={computer} setOnClick={onClickPartSlot} partName={"Видеокарта"}/>),
-      (<PartSlot key={4} iconAddress={RAMIcon} partClassName={PartClassName.RAM} computer={computer} setOnClick={onClickPartSlot} partName={"Оперативная память"}/>),
-      (<PartSlot key={5} iconAddress={PowerIcon} partClassName={PartClassName.PowerUnit} computer={computer} setOnClick={onClickPartSlot} partName={"Блок питания"}/>),
-      (<PartSlot key={6} iconAddress={HDDIcon} partClassName={PartClassName.PrimaryStorage} computer={computer} setOnClick={onClickPartSlot} partName={"Диск"}/>),
-      (<PartSlot key={7} iconAddress={FanIcon} partClassName={PartClassName.CoolingSystem} computer={computer} setOnClick={onClickPartSlot} partName={"Система охлаждения"}/>)
-    ]
-  );
+  const [partSlotList, setPartSlotList] = useState(predefinedPartSlotList(computer, onClickPartSlot));
 
   return (
     <div className="App">
@@ -187,7 +131,6 @@ function App() {
       <PartSelection price={totalCost} partSlotList={partSlotList} />
       <ItemCardHolder itemCardList={itemCardList} itemCardHolderState={itemCardHolderState} /> 
     </div>
-    
   );
 
 }
